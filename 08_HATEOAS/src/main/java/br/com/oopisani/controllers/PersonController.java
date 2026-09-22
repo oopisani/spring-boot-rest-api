555f12cff1feeb9d9242857c24865f9e3aa739c0
Bruna Pisani <anyapisani@gmail.com>
2026-09-22 11:31:51 -0300

    add HATEOAS to the code, reaching the final level of the RESTFUL API maturity model, and update tests acoordingly

---
+++ 08_HATEOAS/src/main/java/br/com/oopisani/controllers/PersonController.java
@@ -0,0 +1,64 @@
+package br.com.oopisani.controllers;
+
+import br.com.oopisani.data.dto.PersonDTO;
+import br.com.oopisani.model.Person;
+import br.com.oopisani.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+
+    // GetMapping é um alias para ex @RequestMapping(method = RequestMethod.POST
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+                            MediaType.APPLICATION_XML_VALUE,
+                            MediaType.APPLICATION_YAML_VALUE})
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+
+    // Não precisamos mais do value pois esse parâmetro do create é passado através do BODY
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+                             MediaType.APPLICATION_XML_VALUE,
+                             MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+                            MediaType.APPLICATION_XML_VALUE,
+                            MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
+                        MediaType.APPLICATION_XML_VALUE,
+                        MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+       service.delete(id);
+       return ResponseEntity.noContent().build();
+    }
+}
